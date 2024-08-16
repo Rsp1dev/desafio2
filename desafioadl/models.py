7e8a4fce0f8c01a8e0b9ecdf74ddc629693a261a
Ruben Soto <rubensotopavez@gmail.com>
2024-08-16 12:19:00 -0400

    Se crearon modelos tarea y subtarea

--- desafioadl/models.py
+++ desafioadl/models.py
@@ -9,7 +9,7 @@ class SubTarea(models.Model):
     id = models.AutoField(primary_key=True)
     descripcion = models.TextField(max_length=400, default='')
     eliminada = models.BooleanField(default=False)
-    tarea_id = models.ForeignKey(Tarea, on_delete=models.CASCADE)
+    tarea_id = models.ForeignKey(Tarea, related_name='subtarea', on_delete=models.CASCADE)
 
 
 
